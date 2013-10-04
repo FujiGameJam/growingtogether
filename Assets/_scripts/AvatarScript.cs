ef4a557be74c0904957519500d73be7d37ffb887
Manu Evans <turkeyman@gmail.com>
2013-10-05 03:35:03 +1000

    Initial tether.

--- Assets/_scripts/AvatarScript.cs
+++ Assets/_scripts/AvatarScript.cs
@@ -10,19 +10,31 @@ public class AvatarScript : MonoBehaviour
 		player3,
 		player4
 	}
-	
+
 	public PlayerID m_playerID = PlayerID.player1;
-	
+
 	public float m_movementSpeed = 5.0f;
 	public float m_runSpeed = 8.0f;
 	public float m_turnSpeed = 5.0f;
-	
+
 	float verticalForce = 0.0f;
-	
+
+	Tether.Node tether;
+
+	public void SetTether(Tether.Node n)
+	{
+		tether = n;
+	}
+
+	public Tether.Node GetTether()
+	{
+		return tether;
+	}
+
 	string Player ()
 	{
 		string result = "";
-		
+
 		switch ( m_playerID )
 		{
 			case PlayerID.player1:
@@ -36,29 +48,29 @@ public class AvatarScript : MonoBehaviour
 				break;
 			case PlayerID.player4:
 				result = "p4";
-				break;			
+				break;
 		}
-		
+
 		return result;
 	}
-	
+
 	// Use this for initialization
 	void Start ()
 	{
-	
+
 	}
-	
+
 	// Update is called once per frame
 	void Update ()
 	{
 		Vector3 inputVector = new Vector3(Input.GetAxis ( Player() + "Horizontal" ), 0.0f, Input.GetAxis ( Player() + "Vertical" ));
 		bool running = false;
-		
+
 		if (inputVector.magnitude > 0.1)
 		{
 			transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (inputVector), Time.deltaTime * m_turnSpeed);
 		}
-		
+
 		if (verticalForce <= 0.0 && Physics.SphereCast (new Ray(transform.position, -Vector3.up), 0.5f, 0.5f))
 		{
 			if (Input.GetButtonDown (Player() + "Jump"))
@@ -83,9 +95,11 @@ public class AvatarScript : MonoBehaviour
 		{
 			verticalForce -= Time.deltaTime * 9.8f;
 		}
-		
+
 		inputVector.y = verticalForce;
-		
+
 		rigidbody.velocity = inputVector * (running ? m_runSpeed : m_movementSpeed);
+
+		rigidbody.velocity += tether.Force();
 	}
 }
